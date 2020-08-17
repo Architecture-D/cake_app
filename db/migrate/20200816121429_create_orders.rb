aa8c6af48651c511cccc428f615f2cdbcfdf23c6
maeda-seina <66904873+maeda-seina@users.noreply.github.com>
2020-08-17 16:00:56 +0900

    Update 20200816121429_create_orders.rb

--- db/migrate/20200816121429_create_orders.rb
+++ db/migrate/20200816121429_create_orders.rb
@@ -3,7 +3,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
     create_table :orders do |t|
       t.integer :customer_id, null: false
       t.string :post_code, null: false
-      t.string :adress, null: false
+      t.string :address, null: false
       t.string :name, null: false
       t.integer :shipping_cost, null: false, default: 800
       t.integer :total_payment, null: false

07a4045f6e4455d40d8f16df7c2ca0454167fec9
vagrant <vagrant@localhost.localdomain>
2020-08-17 06:15:43 +0000

    修正4回目

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 
 	validates :genre_id, presence: true
 	validates :name, presence: true
-	validates :introduction, presence: true, ength:{maximum: 300}
+	validates :introduction, presence: true, length:{ maximum: 300}
 	validates :price, presence: true, numericality:{ only_integer: true}
 
 	attachment :image_id

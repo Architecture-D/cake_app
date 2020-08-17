ca0f1c747770fb8cd3890123737ad10b0cee8cec
maeda-seina <66904873+maeda-seina@users.noreply.github.com>
2020-08-17 16:17:28 +0900

    Update 20200816122006_create_order_products.rb

--- db/migrate/20200816122006_create_order_products.rb
+++ db/migrate/20200816122006_create_order_products.rb
@@ -6,8 +6,7 @@ class CreateOrderProducts < ActiveRecord::Migration[5.2]
       t.integer :purchase_price, null: false
       t.integer :quantity, null: false
       t.integer :make_status, null: false, default: 0
-      t.datetime :created_at, null: false
-      t.datetime :updated_at, null: false
+      
 
       t.timestamps
     end

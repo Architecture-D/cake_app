68304a394fe5e1406b2ba077a6fae34ab7836c7a
Yuki-Ichigo <bzpl1215@gmail.com>
2020-08-17 07:04:23 +0000

    [修正]add model

--- db/migrate/20200816115554_create_destinations.rb
+++ db/migrate/20200816115554_create_destinations.rb
@@ -1,12 +1,13 @@
 class CreateDestinations < ActiveRecord::Migration[5.2]
   def change
     create_table :destinations do |t|
-      t.integer :customer_id
-      t.string :post_code
-      t.string :address
-      t.string :name
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.integer :customer_id, null: false
+      t.string :post_code, null: false
+      t.string :address, null: false
+      t.string :name, null: false
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
+      t.timestamps
     end
   end
 end

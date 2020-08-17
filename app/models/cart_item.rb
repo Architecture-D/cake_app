b8ed1cf7bc7376f60bb6a1fe9029bc0c3e0e47da
Yuki-Ichigo <bzpl1215@gmail.com>
2020-08-17 05:52:26 +0000

    [add]model

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,5 +2,5 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :product
 
-  validates :quantity, presence: true, numerricality:{only_integer: true}
+  validates :quantity, presence: true, numericality:{only_integer: true}
 end

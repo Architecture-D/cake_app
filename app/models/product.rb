62c0d666e45aa27b1e105e1e310408e7c1322603
vagrant <vagrant@localhost.localdomain>
2020-08-17 04:00:40 +0000

    モãモデモデル追加

---
+++ app/models/product.rb
@@ -0,0 +1,16 @@
+class Product < ApplicationRecord
+
+	validates :genre_id, presence: true
+	validates :name, presence: true
+	validates :introduction, presence: true, ength:{maximum: 300}
+	validates :price, presence: true, numerricality:{ only_integer: true}
+
+	attachment :image_id
+
+	has_many :cart_items
+	has_many :customers, through: :cart_items
+	has_many :order_items
+	has_many :orders, through: :order_items
+	belongs_to :genre
+
+end

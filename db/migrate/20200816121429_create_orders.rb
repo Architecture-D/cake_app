6a8ccdab1b6b1f9412c241c7c749fca3a66d8996
maeda-seina <66904873+maeda-seina@users.noreply.github.com>
2020-08-17 16:18:01 +0900

    Update 20200816121429_create_orders.rb

--- db/migrate/20200816121429_create_orders.rb
+++ db/migrate/20200816121429_create_orders.rb
@@ -9,8 +9,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
       t.integer :total_payment, null: false
       t.integer :payment_method, null: false, default: 0
       t.integer :status, null: false, default: 0
-      t.datetime :created_at, null: false
-      t.datetime :updated_at, null: false
+      
 
       t.timestamps null:false
     end

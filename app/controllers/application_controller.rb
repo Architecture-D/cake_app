2c30564e6fe44b5eafb8f007e924f1c006f64c08
vagrant <vagrant@localhost.localdomain>
2020-08-19 09:34:19 +0000

    [add]header-scss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number])
   end
+
+   def after_sign_in_path_for(resource)
+    case resource
+      when Admin
+      admin_home_top_path
+      when Customer
+      products_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
 end

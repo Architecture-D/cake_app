a7c3182cd073038279f3c70a09db2ddc827fbc14
vagrant <vagrant@localhost.localdomain>
2020-08-19 06:14:22 +0000

    [fix]rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,31 @@
 Rails.application.routes.draw do
 
    devise_for :admins
-  
-  devise_for :customers
-  root 'home#top'
-  get 'home/about', to: 'home#about'
-  get '/products', to: 'products#index'
-  resources :customers,only: [:show,:index,:edit,:update,:hide,:delete]
-  resources :cart_items,only: [:create,:index,:destroy_all,:destroy,:update]
-  resources :orders,only: [:new,:confirm,:create,:thank,:index,:show]
-  resources :destinations,only: [:index,:create,:destroy,:edit,:update]
 
+  devise_for :customers, controllers: {
+    registrations: 'customers/registrations',
+    sessions: 'customers/sessions'
+  }
+  # devise_for :customers, skip: [:registration]
+  #  devise_scope :customers do
+  #    get 'customers/sign_up', to: 'devise/registrations#new', as: 'new_customer_registration'
+  #    post 'customers', to:'devise/registrations#create', as: 'customer_registration'
+  # end
+
+  scope module: :customers do
+    root 'home#top'
+    get 'home/about', to: 'home#about'
+    get '/products', to: 'products#index'
+    get '/products/:id', to: 'products#show'
+    resource :customers,only: [:show,:index,:edit,:update,:destroy] do
+      collection do
+        get 'hide'
+      end
+    end
+    resources :cart_items,only: [:create,:index,:destroy_all,:destroy,:update]
+    resources :orders,only: [:new,:confirm,:create,:thank,:index,:show]
+    resources :destinations,only: [:index,:create,:destroy,:edit,:update]
+  end
 
   namespace :admin do
     get 'home/top', to: 'home#top'
@@ -20,5 +35,6 @@ Rails.application.routes.draw do
     resources :order_details,only: [:update]
     resources :orders,only: [:index,:show,:update]
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

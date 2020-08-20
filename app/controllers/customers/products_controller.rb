aecdcd3131256cdc6075a77110cb38575279deb2
vagrant <vagrant@localhost.localdomain>
2020-08-20 16:05:15 +0000

    [add]scss_products

--- app/controllers/customers/products_controller.rb
+++ app/controllers/customers/products_controller.rb
@@ -1,7 +1,15 @@
 class Customers::ProductsController < ApplicationController
   def index
+  	 @genres = Genre.where(is_active: true)
+
+     #「ジャンルのステータス有効になっている」＋「商品のステータスが販売中になっている」商品のみ表示
+  	 @products = Product.joins(:genre).where(is_active: true, genres: { is_active: "true"})
+
+  	 #products = Product.where(genre_id: params[:product][:genre_id],is_active: true)
   end
 
   def show
+     @product = Product.find(params[:id])
   end
+
 end

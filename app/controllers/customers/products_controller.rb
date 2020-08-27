5f94fa6206a7b59abd2f52290920be335e24395b
vagrant <vagrant@localhost.localdomain>
2020-08-27 09:43:54 +0000

    コントローラ

--- app/controllers/customers/products_controller.rb
+++ app/controllers/customers/products_controller.rb
@@ -1,22 +1,9 @@
 class Customers::ProductsController < ApplicationController
 
- # def index
-    # @genres = Genre.where(is_active: true)
-
      #「ジャンルのステータス有効になっている」＋「商品のステータスが販売中になっている」商品のみ表示
      #@products = Product.joins(:genre).where(is_active: true, genres: { is_active: "true"})
 
      #@products = Product.where(genre_id: params[:product][:genre_id],is_active: true)
-
-
-     #省略されてるけどここで　rende indexしてviewに飛ばしてる
-  #end
-
-  #def search?してジャンルID持ってきたあと↑のindexにrenderかな？
-
-  #def index
-    #if params[:genre_id].present?
-      #@products = Product.where(genre_id: params[:product][:genre_id],is_active: true)
 
   def index
       @products_genre_all = Product.where(genre_id: params[:genre_id],is_active: true).page(params[:page])

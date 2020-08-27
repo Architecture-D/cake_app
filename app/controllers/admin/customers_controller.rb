81e4a5d1ddbdefd05c80d6dde9d916a2dc7f6ce8
Yuki-Ichigo <bzpl1215@gmail.com>
2020-08-27 10:07:53 +0000

    [修正]admin_customer_コントローラー

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CustomersController < ApplicationController
 
-before_action :authenticate_customer!
+before_action :authenticate_admin!
   
   def index
   	@customers = Customer.all.page(params[:page])

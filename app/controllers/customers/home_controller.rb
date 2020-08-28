4d155f8232e4e672e0f41613aa864c7307a07b93
Yuki-Ichigo <bzpl1215@gmail.com>
2020-08-28 07:56:39 +0000

    [修正]退会後のcustomerがログインできない

--- app/controllers/customers/home_controller.rb
+++ app/controllers/customers/home_controller.rb
@@ -1,7 +1,7 @@
 class Customers::HomeController < ApplicationController
   def top
   	@products = Product.joins(:genre).where(is_active: true, genres: { is_active: "true"})
-  	 @genres = Genre.where(is_active: true)
+  	@genres = Genre.where(is_active: true)  	
   end
 
   def about
